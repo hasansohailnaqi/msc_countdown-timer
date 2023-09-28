a6596f8582ac3ee313661dff1cc58c40ca6b417f
hasansohailnaqi <hasansohail793@gmail.com>
2023-09-28 07:21:08 +0500

    processes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import { differenceInSeconds } from "date-fns";
 import inquirer from "inquirer";
 const res = await inquirer.prompt({
-    type: "input",
+    type: "number",
     name: "userInput",
     message: "please enter the amount of second",
 });
-let val = res.userInput;
-console.log(val);
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const IntervalTime = new Date(intTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(IntervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Time has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
